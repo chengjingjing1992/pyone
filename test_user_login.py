1de821a0521bf4ba918ffc1a93d0eeb8f4dc9cde
chengjingjing1992 <2629704685@qq.com>
2019-07-28 21:47:58 +0800

    从excel 读取数据

--- test_user_login.py
+++ test_user_login.py
@@ -2,6 +2,8 @@
 import unittest
 import requests
 import test_globle
+import json
+import read_excel
 from http.cookiejar import CookieJar,LWPCookieJar
 from urllib.request import Request,urlopen,HTTPCookieProcessor,build_opener
 class TestUserLogin(unittest.TestCase):
@@ -14,13 +16,20 @@ class TestUserLogin(unittest.TestCase):
 
     def test_user_login_normal(self):# 一条测试用例，必须test_开头
 
-        url="http://servemobiletest.kuaishouax.com/ApiLogin/AjaxUserLogin"
-        params={
-            "userType":"1",
-            "telphone":"13585658502",
-            "password":"123456"
-        }
-
+        # url="http://servemobiletest.kuaishouax.com/ApiLogin/AjaxUserLogin"
+        # params={
+        #     "userType":"1",
+        #     "telphone":"13585658502",
+        #     "password":"123456"
+        # }
+        data_list = read_excel.excel_to_list("test_user_data.xlsx", "TestUserLogin")  # 读取excel，TestUserLogin工作簿的所有数据
+        case_data = read_excel.get_test_data(data_list, 'test_user_login_normal')  # 查找用例'test_user_login_normal'的数据
+        url=case_data["url"]
+        print(url)
+        params=case_data["data"]
+        # 转成json格式
+        params=dict(json.loads(params))
+        # print(type(params))
         r=requests.request("post",url,params=params)
         self.assertIn("true",r.text) # 断言
         # 给全局变量cookie 赋值
